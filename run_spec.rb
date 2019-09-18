a7d61113d3608d8613e0039ae4988fd7319ba168
Kartik Aiyar <kartik.aiyar@riverbed.com>
2019-09-17 18:17:02 -0700

    updated input argument for serial

--- run_spec.rb
+++ run_spec.rb
@@ -10,6 +10,7 @@ options[:telnet] = false
 options[:skip_api]=false
 options[:remote_report]= true
 options[:remote_srvr]="10.100.185.250"
+options[:serial]=nil
 
 OptionParser.new do |opts|
   opts.on("--env ENVIRONMENT"){|obj| options[:env]=obj}
@@ -23,7 +24,19 @@ OptionParser.new do |opts|
   opts.on("--telnet TELNET"){|obj| options[:telnet]=obj}
   opts.on("--skip_api TELNET"){|obj| options[:skip_api]=obj}
   opts.on("--remote_report RemoteREPORTINGENABLED"){|obj| options[:remote_report]=obj}
+  opts.on("--serial SERIAL","Please Provide an Array Serial"){|obj| options[:serial] = obj}
 end.parse!
+
+
+if options[:serial] 
+  if options[:serial] != 'none'
+    array_serial = options[:serial].to_sym
+  else
+    array_serial = nil
+  end
+else 
+  array_serial = nil
+end
 
 settings={
   env: options[:env],
@@ -35,7 +48,8 @@ settings={
   remote_report: options[:remote_report],
   project_id: options[:project_id],
   testcycle_id: options[:testcycle_id],
-  release_id: options[:release_id]
+  release_id: options[:release_id],
+  array_serial: array_serial
  }
 spec = options[:spec]
 #spec= "TS_Profiles/TC_Profile_01_spec.rb"
